f67360d3b62c4b3c8ee0484d13be88b0fc58c8e7
lreikofski <lreikofski2@huskers.unl.edu>
2021-08-26 16:51:19 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Lake Reikofski
+ * Date: 2021/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+  #include <stdlib.h>
+  #include <stdio.h>
+
+  int main(int argc, char **argv) {
+
+  printf("Lake!\n");
+  printf("Computer Engineering");
+
+  return 0;
+}
